9587beef1f4d3230c381df639f00e44ecfbccb2a
Beast1005 <84617537+Beast1005@users.noreply.github.com>
2023-07-31 19:03:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request
+import backend_config
+
+app=Flask(__name__)
+
+@app.route("/running",methods=["POST"])
+def running_config():
+    name_file=request.args["file_name"]
+    device_details_list=backend_config.get_device_details(name_file)
+    result=backend_config.running_config(device_details_list)
+    return result
+    
+@app.route("/interface_list",methods=["POST"])
+def interface_list():
+    name_file=request.args["file_name"]
+    device_details_list=backend_config.get_device_details(name_file)
+    result=backend_config.interface_list(device_details_list)
+    return result
+    
+@app.route("/int_config",methods=["POST"])
+def interface_config():
+    name_file=request.args["file_name"]
+    device_details_list=backend_config.get_device_details(name_file)
+    number_of_interfaces=request.args["number_of_interfaces"]
+    interface_details_list=[]
+    print(number_of_interfaces)
+    for i in range(int(number_of_interfaces)):
+        each_interface=request.args[f"interface{i}"]
+        interface_details_list.append(each_interface)
+    result=backend_config.interface_config(device_details_list,interface_details_list)
+    return result
+    
+if __name__=="__main__":
+    app.run()
---
+++ backend_config.py
@@ -0,0 +1,75 @@
+from ncclient import manager
+from xml.dom.minidom import parseString
+import config
+
+def running_config(device_details_list):
+    for each_device in device_details_list:
+        print(each_device)
+        ssh=manager.connect(**each_device)
+        result=ssh.get_config(source="running")
+        with open(each_device["host"],"w") as file:
+            file.write(convert_to_pretty_config(result))
+    return f"config taken for all devices"
+
+def convert_to_pretty_config(result):
+    return parseString(result.xml).toprettyxml()
+
+def get_device_details(file_name):
+    device_details_list=[]
+    with open(file_name,"r") as file:
+        for each_ip in file:
+            each_device_details={
+                    "host":each_ip.strip(),
+                    "port":config.PORT,
+                    "username":config.USERNAME,
+                    "password":config.PASSWORD,
+                    "hostkey_verify":False
+                }
+            device_details_list.append(each_device_details)
+    return device_details_list
+
+def interface_list(device_details_list):
+    interface_filter="""
+    <filter>
+        <interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces>
+            <interface></interface>
+        </interfaces-state>
+    </filter>
+    """
+    for each_device in device_details_list:
+        with manager.connect(**each_device) as ssh:
+            result = ssh.get(filter=('xpath', '/interfaces-state'))
+            interfaces = result.data.xpath("//*[local-name()='interface']")
+            print("List of Interfaces:")
+            for interface in interfaces:
+                name = interface.find('.//{urn:ietf:params:xml:ns:yang:ietf-interfaces}name').text
+                print(f"- {name}")
+    return "success"
+
+def interface_config(device_details_list,interface_details):
+    for each_device in device_details_list:
+        ssh = manager.connect(**each_device)
+        for each_interface in interface_details:
+            each_word=each_interface.split(",")
+            payload=f"""
+            <config>
+            <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+                <interface>
+                    <name>{each_word[0]}</name>
+                    <description>{each_word[3]}</description>
+                    <type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:ethernetCsmacd</type>
+                    <enabled>true</enabled>
+                    <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
+                        <address>
+                            <ip>{each_word[1]}</ip>
+                            <netmask>{each_word[2]}</netmask>
+                        </address>
+                    </ipv4>
+                    <ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip"/>
+                </interface>
+            </interfaces>
+            </config>
+            """
+            print(payload)
+            result=ssh.edit_config(payload,target="running")
+    return "success"
