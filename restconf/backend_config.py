7cce46ebc60b69a173cbed59928b6783805c2ea6
Beast1005 <gandam.sagar1005@gmail.com>
2023-08-13 09:41:53 +0530

    commit

--- restconf/backend_config.py
+++ restconf/backend_config.py
@@ -8,6 +8,8 @@ import json
 from datetime import datetime
 import requests
 import config
+from requests.auth import HTTPBasicAuth
+import urllib3
 
 
 def take_backup(file_name):
@@ -22,14 +24,15 @@ def take_backup(file_name):
     """
     with open(file_name, "r",encoding="utf-8") as file:
         for each_line in file:
-            url = f"https://{each_line}/restconf/data/Cisco-IOS-XE-native:native"
+            # urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+            url = f"https://{each_line.strip()}:443/restconf/data/Cisco-IOS-XE-native:native"
             response = requests.get(
                 url,
                 headers=config.ACCEPT_HEADER,
-                auth=(config.USERNAME, config.PASSWORD),
+                auth=HTTPBasicAuth(config.USERNAME, config.PASSWORD),
                 verify=False,
                 timeout=30
             )
             now = datetime.now()
-            with open(f"{each_line}_{now.strftime('_%m_%d_%Y_%H_%M_%S')}","a",encoding="utf-8") as file:
+            with open(f"{each_line.strip()}_{now.strftime('_%m_%d_%Y_%H_%M_%S')}","a",encoding="utf-8") as file:
                 json.dump(response.json(), file, indent=2)
--- restconf/config.py
+++ restconf/config.py
@@ -1,6 +1,8 @@
 PORT=443
-USERNAME="developer"
-PASSWORD="lastorangerestoreball8876"
+# USERNAME="developer"
+# PASSWORD="lastorangerestoreball8876"
+USERNAME="admin"
+PASSWORD="cisco"
 ACCEPT_HEADER={
     'accept':'application/yang-data+json'
 }
