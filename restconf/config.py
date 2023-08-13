867ccd6555524e6faa51b9321f73d662eaa687df
Beast1005 <gandam.sagar1005@gmail.com>
2023-08-13 12:22:38 +0530

    commit

--- restconf/config.py
+++ restconf/config.py
@@ -1,8 +1,9 @@
 PORT=443
 USERNAME="admin"
 PASSWORD="C1sco12345"
-# USERNAME="admin"
-# PASSWORD="cisco"
 ACCEPT_HEADER={
     'accept':'application/yang-data+json'
 }
+CONTENT_TYPE_HEADER={'Content-Type': 'application/yang-data+json'}
+# USERNAME="admin"
+# PASSWORD="cisco"
